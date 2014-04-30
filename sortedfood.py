886439fb8a1a4356c8e866fbfa3c1c738468e1fa
Marco don Kaulea <DonMarco42@gmail.com>
2014-04-30 23:47:44 +0200

    Extract recipe and further links from recipe page

--- sortedfood.py
+++ sortedfood.py
@@ -16,21 +16,49 @@ Usage:
 import docopt as dopt
 import bs4
 import requests
+import pprint
+
+
+def retrieve_recipe_page(page_name):
+    response = requests.get(
+        "http://sortedfood.com/support/client/ajax/pageload.php?" +
+        "page={0}".format(page_name))
+    if response.status_code == 200:
+        return response.text
+    else:
+        return ""
+
+
+def parse_recipe_page(page_name, page):
+    store = {}
+    soup = bs4.BeautifulSoup(page)
+    store["title"] = soup.find(id="titleLink").text
+    store["name"] = page_name
+    store["bodyText"] = soup.find(id="pageBodyText").text
+    store["ingredients"] = [li.text for li in soup.find(
+                            id="expansionMoreBody").ul.find_all("li")]
+    instructions = soup.find(id="expansionMoreBody").p.text.split("\n\n")
+    if len(instructions) == 2:
+        store["instructions"] = instructions[0]
+        store["portions"] = instructions[1]
+    elif len(instructions) == 1:
+        store["instuctions"] = instructions[0]
+    return store
+
+
+def extract_recipe_links(recipe_page):
+    soup = bs4.BeautifulSoup(recipe_page)
+    links = soup.find_all("a")
+    link_filter = lambda x: x["class"] == ["active"]
+    return [a["href"].replace("/", "") for a in links if link_filter(a)]
 
 
 def main(args):
-    mainResponse = requests.get("http://sortedfood.com")
-    headers = {"Referer": "http://sortedfood.com/",
-               "X-Requested-With": "XMLHttpRequest"}
-    response = requests.get(
-        "http://sortedfood.com/support/" +
-        "client/ajax/pageload.php?page=katsucurry",
-        headers=headers,
-        cookies=mainResponse.cookies)
-    print(response)
-    soup = bs4.BeautifulSoup(response.text)
-    print(soup.find(id="titleLink").text)
-    print(soup.find(id="expansionMoreBody"))
+    response = retrieve_recipe_page("katsucurry")
+    recipe = parse_recipe_page("katsucurry", response)
+    further_links = extract_recipe_links(response)
+    pprint.pprint(recipe)
+    pprint.pprint(further_links)
 
 
 if __name__ == "__main__":
