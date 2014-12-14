c8db799c403d17405debca44ed06b8768d11b37f
Marco don Kaulea <DonMarco42@gmail.com>
2014-12-14 20:35:05 +0100

    Add method documentation

--- sortedfood_api.py
+++ sortedfood_api.py
@@ -11,30 +11,64 @@ import requests
 import json
 
 
-def get_recipe(page_name,
+def get_recipe(recipe_id,
                ingredients=True,
                instructions=True,
                session=None):
+    """Get recipe from id
+
+    Args:
+        recipe_id (string): Numeric recipe id
+        ingredients (bool): Request list of ingredients with the recipe.
+            Defaults to True
+        instructions (bool): Request list of instructions with the recipe.
+            Defaults to True
+        session (Requests.Session): HTTP session to be used for the request.
+            Defaults to None. When no session is provied a new one will be
+            created and used.
+
+    Returns:
+        string: Response parsed as json
+
+    Raises:
+        Exception: It the status code is not 200
+
+    """
     if session is None:
         session = requests.Session()
-    payload = {'recipeid':  page_name,
+    payload = {'recipeid':  recipe_id,
                'getIngredients': 'true' if ingredients else 'false',
                'getInstructions': 'true' if instructions else 'false'}
     response = session.get('https://cms.sortedfood.com/apiRecipe/getRecipe',
                            params=payload)
     if response.status_code == 200:
+        response.encoding = 'utf-8'
         return json.loads(response.text)
     else:
-        return {}
+        raise requests.HTTPError(response)
 
 
 def get_categories(session=None):
+    """Get all categories
+
+    Args:
+        session (Requests.Session): HTTP session to be used for the request.
+            Defaults to None. When no session is provied a new one will be
+            created and used.
+
+    Returns:
+        dict: containing categories addressed by category id
+
+    Raises:
+        requests.HTTPError: If the status code is not 200
+
+    """
     if session is None:
         session = requests.Session()
     response = session.get(
         'https://cms.sortedfood.com/apiRecipe/getCategoryMenu')
     if response.status_code != 200:
-        raise requests.HTTPError()
+        raise requests.HTTPError(response)
     response.encoding = 'utf-8'
     data = json.loads(response.text)
     categories = {}
@@ -48,30 +82,68 @@ def get_categories(session=None):
     return categories
 
 
-def get_recipies_from_category(category,
+def get_recipies_from_category(category_id,
                                usertype=1,
                                page=0,
                                count=0,
                                session=None):
+    """Get recipe excerpt from category
+
+    Args:
+        category_id (string): Numeric category id
+        usertype (int): 1 for sortedfood, 2 for everyone else. Defaults to 1.
+        page (int): Which page to retrieve. Defaults to 0.
+        count(int): How many items per page. Defaults to 0, meaning all.
+        session (Requests.Session): HTTP session to be used for the request.
+            Defaults to None. When no session is provied a new one will be
+            created and used.
+
+    Returns:
+        string: Response parsed as json
+
+    Raises:
+        requests.HTTPError: If the status code is not 200
+
+    """
     if session is None:
         session = requests.Session()
-    payload = {'categoryId': category,
+    payload = {'categoryId': category_id,
                'usertype': usertype,
                'page': page,
                'offset': count}
     response = session.get(
         'https://cms.sortedfood.com/apiRecipe/getFeaturedByUsertype',
         params=payload)
-    if response.status_code == 200:
-        return json.loads(response.text)
+    if response.status_code != 200:
+        raise response.HTTPError(response)
+    response.encoding = 'utf-8'
+    return json.loads(response.text)
 
 
-def get_recipe_ids_from_category(category,
+def get_recipe_ids_from_category(category_id,
                                  usertype=1,
                                  page=0,
                                  count=0,
                                  session=None):
-    content = get_recipies_from_category(category,
+    """Get recipe ids from a category
+
+    Args:
+        category_id (string): Numeric category id
+        usertype (int): 1 for sortedfood, 2 for everyone else. Defaults to 1.
+        page (int): Which page to retrieve. Defaults to 0.
+        count(int): How many items per page. Defaults to 0, meaning all.
+        session (Requests.Session): HTTP session to be used for the request.
+            Defaults to None. When no session is provied a new one will be
+            created and used.
+
+    Returns:
+        set: Containing unique recipe ids.
+
+    Raises:
+        requests.HTTPError: If the status code is not 200
+
+    """
+    content = get_recipies_from_category(category_id,
                                          usertype,
                                          page,
                                          count,
